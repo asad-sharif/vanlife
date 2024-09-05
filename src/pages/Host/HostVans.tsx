cd77aa8fdb5729861bb069684793325681d2b34e
asad-sharif <asadsharif1270@gmail.com>
2024-09-05 23:53:49 +0500

    same

--- src/pages/Host/HostVans.tsx
+++ src/pages/Host/HostVans.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../../server'
 
 
 function HostVans() {
-    const [vans, setVans] = React.useState([])
 
     React.useEffect(() => {
         async function fetchData() {
